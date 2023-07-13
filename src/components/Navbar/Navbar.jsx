be885be444579bc000d071df1e7f2b072f01226e
sarvarmarimboyev <75431632+sarvarmarimboyev@users.noreply.github.com>
2023-07-13 22:53:05 +0500

    hh

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -76,7 +76,6 @@
 // };
 
 // export default Navbar;
-
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
@@ -88,9 +87,11 @@ import Logo from "../../assets/Logo.png";
 const Navbar = () => {
   const { t, i18n } = useTranslation();
   const [drawerVisible, setDrawerVisible] = useState(false);
+  const [collectVisible, setCollectVisible] = useState(false);
 
   const toggleDrawer = () => {
     setDrawerVisible(!drawerVisible);
+    setCollectVisible(!drawerVisible);
   };
 
   const changeLanguage = (language) => {
@@ -104,47 +105,50 @@ const Navbar = () => {
           <img src={Logo} alt="" />
         </Link>
       </div>
-      <ul className={`navbar-items ${drawerVisible ? "active" : ""}`}>
-         {drawerVisible ? (
-          <Button
-            className="navbar-button close-button"
-            icon={<CloseOutlined />}
-            onClick={toggleDrawer}
-          />
-        ) : null} 
-        <li>
-          <Link to="/">{t("navbar.item1")}</Link>
-        </li>
-        <li>
-          <Link to="/news">{t("navbar.item2")}</Link>
-        </li>
-        <li>
-          <Link to="/services">{t("navbar.item3")}</Link>
-        </li>
-        <li>
-          <Link to="/about">{t("navbar.item4")}</Link>
-        </li>
-        <li>
-          <Link to="/contact">{t("navbar.item5")}</Link>
-        </li>
-        <li>
-          <Link to="/partners">{t("navbar.item6")}</Link>
-        </li>
-        <li>
-          <Link to="/docs">{t("navbar.item7")}</Link>
-        </li>
-      
-      </ul>
-
+      <div className={`collect ${collectVisible ? "active" : ""}`}>
+        <ul className={`navbar-items ${drawerVisible ? "active" : ""}`}>
+          {drawerVisible ? (
+            <Button
+              className="navbar-button close-button"
+              icon={<CloseOutlined />}
+              onClick={toggleDrawer}
+            />
+          ) : null}
+          <li>
+            <Link to="/">{t("navbar.item1")}</Link>
+          </li>
+          <li>
+            <Link to="/news">{t("navbar.item2")}</Link>
+          </li>
+          <li>
+            <Link to="/services">{t("navbar.item3")}</Link>
+          </li>
+          <li>
+            <Link to="/about">{t("navbar.item4")}</Link>
+          </li>
+          <li>
+            <Link to="/contact">{t("navbar.item5")}</Link>
+          </li>
+          <li>
+            <Link to="/partners">{t("navbar.item6")}</Link>
+          </li>
+          <li>
+            <Link to="/docs">{t("navbar.item7")}</Link>
+          </li>
+        </ul>
+      </div>
       <select
-         value={i18n.language}
-         onChange={(e) => changeLanguage(e.target.value)}
-       >
-         <option value="ru">Ru</option>
-         <option value="uz">Uz</option>
-       </select>    <Button
+        value={i18n.language}
+        onChange={(e) => changeLanguage(e.target.value)}
+      >
+        <option value="ru">Ru</option>
+        <option value="uz">Uz</option>
+      </select>
+      <Button
         color="white"
-        className={`navbar-button menu-button ${drawerVisible ? "hidden" : ""}`}
+        className={`navbar-button menu-button ${
+          drawerVisible ? "hidden" : ""
+        }`}
         icon={<MenuOutlined />}
         onClick={toggleDrawer}
       />
