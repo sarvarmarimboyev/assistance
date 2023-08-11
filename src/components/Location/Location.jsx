157092c7cb4104abe75776a97e21a86490ba0cc3
sarvarmarimboyev <sarvar.marimboyev@mail.ru>
2023-08-11 21:58:32 +0500

    last

--- src/components/Location/Location.jsx
+++ src/components/Location/Location.jsx
@@ -3,9 +3,9 @@ import { useTranslation } from "react-i18next";
 import "./location.css";
 export const Location = () => {
   const { t } = useTranslation();
-
   return (
-    <>
+  <>
+  
       <div className="container">
         <div className="location__main">
           <div className="location__left">
@@ -33,16 +33,17 @@ export const Location = () => {
             </div>
           </div>
           <div className="location__right">
-            <iframe
-              title="Google Maps Location"
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1499.080600384637!2d69.20836998882983!3d41.28359599282833!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38ae8b36a50c9793%3A0x995b33e6f8b66565!2z0JrQrdCY0KEgItCj0LfQsdC10LrQuNC90LLQtdGB0YIi!5e0!3m2!1sru!2s!4v1688489963379!5m2!1sru!2s"
-              width="100% "
-              height="100%"
-              style={{ border: "0" }}
-              allowfullscreen=""
-              loading="lazy"
-              referrerpolicy="no-referrer-when-downgrade"
-            ></iframe>
+          <iframe
+  title="Google Maps Location"
+  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1499.080600384637!2d69.20836998882983!3d41.28359599282833!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38ae8b36a50c9793%3A0x995b33e6f8b66565!2z0JrQrdCY0KEgItCj0LfQsdC10LrQuNC90LLQtdGB0YIi!5e0!3m2!1sru!2s!4v1688489963379!5m2!1sru!2s"
+  width="100%"
+  height="100%"
+  style={{ border: "0" }}
+  allowFullScreen={true}
+  loading="lazy"
+  referrerPolicy="no-referrer-when-downgrade"
+></iframe>
+
           </div>
         </div>
       </div>
